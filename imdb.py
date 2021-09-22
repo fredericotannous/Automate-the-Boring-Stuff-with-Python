7162feb80c312e685ff91874b2c704bc747c55bb
Frederico Tannous <62360313+fredericotannous@users.noreply.github.com>
2021-09-22 19:28:37 -0300

    Add files via upload

--- imdb.py
+++ imdb.py
@@ -1,5 +1,15 @@
 from bs4 import BeautifulSoup
-import requests
+import requests, openpyxl
+
+#create an excel file
+excel = openpyxl.Workbook()
+sheet = excel.active
+
+#changing sheet title
+sheet.title = 'Top Rated Movies'
+
+#add the columns headers
+sheet.append(['Rank', 'Movie Name', 'Release Date', 'IMDB Rating'])
 
 try:
     source = requests.get('https://www.imdb.com/chart/top/')
@@ -11,13 +21,24 @@ try:
     soup = BeautifulSoup(source.text, 'html.parser')
     
     movies = soup.find('tbody', class_='lister-list').find_all('tr')
-    print(len(movies))
+    #print(len(movies))
 
+    #.a access the tag <a, while .text gets the text on the referenced tag
     for movie in movies:
-        print(movie)
+        
+        name = movie.find('td', class_='titleColumn').a.text     
 
+        rank = movie.find('td', class_='titleColumn').get_text(strip=True).split('.')[0]   
 
+        year = movie.find('td', class_='titleColumn').span.text.strip('()')
+
+        rating = movie.find('td', class_='ratingColumn imdbRating').strong.text
+        
+        print(rank, name, year, rating)
+        sheet.append([rank, name, year, rating])
 
 
 except Exception as e:
     print(e)
+
+excel.save('Top Rated Movies by IMDB')  
